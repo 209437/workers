8db4fd5798a71bb8d82d462645a6e5853c367ffd
209437 <209437@edu.p.lodz.pl>
2019-12-03 16:18:13 +0100

    added worker model

---
+++ src/main/java/com/weeia/workers/WorkerModel.java
@@ -0,0 +1,13 @@
+package com.weeia.workers;
+
+import lombok.Data;
+
+@Data
+public class WorkerModel {
+    private int id;
+    private String title;
+    private  String name;
+    private  String surname;
+    private  String institute;
+    private  String url;
+}
